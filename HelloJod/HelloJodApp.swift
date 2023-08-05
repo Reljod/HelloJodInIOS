c3d9fa8a9a2e0ce420cab1c7b4987eb3f6949a04
reljod <oretareljod@gmail.com>
2023-08-05 23:59:30 +0800

    Initial Commit

---
+++ HelloJod/HelloJodApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloJodApp.swift
+//  HelloJod
+//
+//  Created by Reljod Oreta on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloJodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
